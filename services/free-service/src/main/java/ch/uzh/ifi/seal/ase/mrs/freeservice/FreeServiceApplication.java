f78ab48377d6a6fa90c535fc56f2dc7f1977030c
Noah Chavannes <noah.chavannes93@gmail.com>
2021-03-16 18:19:56 +0100

    init microservices with boilerplate projects

---
+++ services/free-service/src/main/java/ch/uzh/ifi/seal/ase/mrs/freeservice/FreeServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.seal.ase.mrs.freeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeServiceApplication.class, args);
+	}
+
+}
---
+++ services/free-service/src/test/java/ch/uzh/ifi/seal/ase/mrs/freeservice/FreeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.seal.ase.mrs.freeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
