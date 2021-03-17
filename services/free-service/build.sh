2d1fcfc6600b738b9f4676489f17950af4080612
Noah Chavannes <noah.chavannes93@gmail.com>
2021-03-17 12:08:44 +0100

    try extracting version

--- services/free-service/build.sh
+++ services/free-service/build.sh
@@ -1,3 +1,5 @@
 echo $DOCKER_USERNAME
-echo mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout
+mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout
 echo $version2
+OUTPUT=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "${OUTPUT}"
