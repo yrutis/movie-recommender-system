a08f4776ececba437677d54dc76f0d609d811db4
Noah Chavannes <noah.chavannes93@gmail.com>
2021-03-17 12:25:58 +0100

    try extracting version

--- services/free-service/build.sh
+++ services/free-service/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 MVN_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
-echo "${MVN_VERSION}"
+mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+mvn sonar:sonar -Dsonar.projectKey=mrs-free-service -Dsonar.host.url=https://sonaruzh.dev.eng.c-alm.ch -Dsonar.login=$SONAR_TOKEN
+docker build -t noahcha/msr-free-service .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push noahcha/msr-free-service:${MVN_VERSION} && docker push noahcha/msr-free-service:latest
