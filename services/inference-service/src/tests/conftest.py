222a42e96c7c07214010a42ebc50b3db9373864f
yrutis <yves.rutishauser@uzh.ch>
2021-03-29 11:23:44 -0600

    init setup of tests with pytest

---
+++ services/inference-service/src/tests/conftest.py
@@ -0,0 +1,12 @@
+# src/tests/conftest.py
+
+import pytest
+
+from src import app
+
+
+@pytest.fixture(scope='module')
+def test_app():
+    app.config.from_object('src.config.TestingConfig')
+    with app.app_context():
+        yield app  # testing happens here
