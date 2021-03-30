59a8f27753c92b628344b90e436969ebd449f632
yrutis <yves.rutishauser@uzh.ch>
2021-03-30 09:48:14 -0600

    create build.sh for python

---
+++ services/inference-service/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PYTHON_VERSION=$(python _version.py)
+IMAGE_NAME=noahcha/mrs-inference-service
+sonar-scanner -Dsonar.projectKey=mrs-inference-service -Dsonar.sources=. -Dsonar.host.url=https://sonaruzh.dev.eng.c-alm.ch -Dsonar.login=$SONAR_TOKEN
+docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME "${IMAGE_NAME}:latest"
+docker tag $IMAGE_NAME "${IMAGE_NAME}:${PYTHON_VERSION}"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push "${IMAGE_NAME}:${PYTHON_VERSION}" && docker push "${IMAGE_NAME}:latest"
