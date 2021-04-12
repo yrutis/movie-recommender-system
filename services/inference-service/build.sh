a4fb8b3c9ab284e051eafbe4a735ffa3abaaa8fa
yrutis <yves.rutishauser@uzh.ch>
2021-04-12 10:55:22 -0600

    fixed pytest coverage report generation

--- services/inference-service/build.sh
+++ services/inference-service/build.sh
@@ -9,9 +9,11 @@ docker tag $IMAGE_NAME "${IMAGE_NAME}:${PYTHON_VERSION}"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "${IMAGE_NAME}:${PYTHON_VERSION}" && docker push "${IMAGE_NAME}:latest"
 # Test
+python3.9 -m venv env
+source env/bin/activate
 pip install -r requirements.txt
-python pytest "src/tests" -p no:warnings
-python pytest -v -o junit_family=xunit1 --cov=src --cov-report xml:test-results/coverage.xml --junitxml=test-results/results.xml
+pytest "src/tests" -p no:warnings
+pytest -v -o junit_family=xunit1 --cov=src --cov-report xml:test-results/coverage.xml --junitxml=test-results/results.xml
 sonar-scanner -Dsonar.projectKey=mrs-inference-service -Dsonar.sources=. -Dsonar.host.url=https://sonaruzh.dev.eng.c-alm.ch -Dsonar.login=$SONAR_TOKEN -Dsonar.language=py -Dsonar.source=src -Dsonar.sourceEncoding=UTF-8 -Dsonar.python.xunit.reportPath=test-results/results.xml -Dsonar.python.coverage.reportPaths=test-results/coverage.xml -Dsonar.python.coveragePlugin=cobertura -Dsonar.exclusions=src/tests/**,test-results/**,coverage-reports/htmlcov
 #flake8 src
 #black src --check
