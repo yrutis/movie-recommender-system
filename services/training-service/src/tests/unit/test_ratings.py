21cd5defb741e8387e7f5f2135aebeebbdcc8cb4
yrutis <yves.rutishauser@uzh.ch>
2021-05-27 16:10:05 +0200

    correct numpy version in requirements file; add users.csv and ratings.csv in git repo for testing purposes

--- services/training-service/src/tests/unit/test_ratings.py
+++ services/training-service/src/tests/unit/test_ratings.py
@@ -1,10 +1,8 @@
 from src.models.ratings import Rating
-import numpy
 
 
 def test_create_rating():
     """
     test if a rating can be initalized
     """
-    print(numpy.__version__)
     Rating(rating=1, user_id=1, tmdb_id=1)
